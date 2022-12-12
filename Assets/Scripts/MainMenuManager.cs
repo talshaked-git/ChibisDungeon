0101361b11c6ac156d55f45cff9be2c356dddc74
Rafael Azriaiev <84143075+SubZeroX9@users.noreply.github.com>
2022-12-12 13:17:44 +0200

    CD-35 Login register feature (#1)
    * CD-4 Added Firebase SDK
    
    * CD-4 Added Register UI
    
    * CD-4 Fix RegisterBtn UI
    
    * CD-5 added Login UI
    
    * CD-6 Added Main Menu Scene with SignOut Button
    
    * CD-5 Added Logic To Move Between Login And Register UI
    
    * CD-5 Added Logic To move Between Login Scene To Main Menu Scene
    
    * CD-4 CD-5 Added Logic for Registration with email verification and Login with auto Login with Firebase
    
    * added new Keystore and google service conf
    
    * resolve dependancy issues
    
    * cd-5 added GoogleLogin
    
    * CD-4 CD-5 Fixed UI Screen Resize

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public static MainMenuManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SignOut()
+    {
+        FireBaseManager.instance.SignOut();
+    }
+}
