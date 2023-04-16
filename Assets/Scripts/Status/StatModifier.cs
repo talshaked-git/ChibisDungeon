2b45ea1ae47fcef619e73a8bfe89d8ac3b619ba4
Rafael Azriaiev <rafael.azrv@gmail.com>
2023-04-16 15:47:44 +0300

    added Documentation to StatModifiers

--- Assets/Scripts/Status/StatModifier.cs
+++ Assets/Scripts/Status/StatModifier.cs
@@ -1,12 +1,19 @@
 using System;
 using System.Collections.Generic;
 
+/// <summary>
+/// StatModType enumeration provides different types of stat modifiers.
+/// </summary>
 public enum StatModType
 {
     Flat = 100,
     PercentAdd = 200,
     PercentMult = 300,
 }
+
+/// <summary>
+/// The StatModifier class represents a stat modifier that can be applied to various game stats.
+/// </summary>
 public class StatModifier
 {
     public readonly float Value;
@@ -14,7 +21,13 @@ public class StatModifier
     public readonly int Order;
     public readonly object Source;
 
-
+    /// <summary>
+    /// Constructs a StatModifier with the specified value, type, order, and source.
+    /// </summary>
+    /// <param name="value">The float value of the stat modifier.</param>
+    /// <param name="type">The StatModType enumeration value representing the type of stat modifier.</param>
+    /// <param name="order">The int order in which the stat modifier should be applied.</param>
+    /// <param name="source">The object source of the stat modifier.</param>
     public StatModifier(float value, StatModType type, int order, object source)
     {
         Value = value;
@@ -23,10 +36,26 @@ public class StatModifier
         Source = source;
     }
 
+    /// <summary>
+    /// Constructs a StatModifier with the specified value and type, using the default order and a null source.
+    /// </summary>
+    /// <param name="value">The float value of the stat modifier.</param>
+    /// <param name="type">The StatModType enumeration value representing the type of stat modifier.</param>
     public StatModifier(float value, StatModType type) : this(value, type, (int)type, null) { }
 
+    /// <summary>
+    /// Constructs a StatModifier with the specified value, type, and order, using a null source.
+    /// </summary>
+    /// <param name="value">The float value of the stat modifier.</param>
+    /// <param name="type">The StatModType enumeration value representing the type of stat modifier.</param>
+    /// <param name="order">The int order in which the stat modifier should be applied.</param>
     public StatModifier(float value, StatModType type, int order) : this(value, type, order, null) { }
 
-    public StatModifier(float value, StatModType type, object source) : this(value, type, (int)type, source) {}
-
+    /// <summary>
+    /// Constructs a StatModifier with the specified value, type, and source, using the default order.
+    /// </summary>
+    /// <param name="value">The float value of the stat modifier.</param>
+    /// <param name="type">The StatModType enumeration value representing the type of stat modifier.</param>
+    /// <param name="source">The object source of the stat modifier.</param>
+    public StatModifier(float value, StatModType type, object source) : this(value, type, (int)type, source) { }
 }
